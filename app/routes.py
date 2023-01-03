d81bdc224f5667b574772c0b1dbf2ece7623cdd4
Valentina Ruiz <v.zruiz06@gmail.com>
2023-01-03 12:14:51 -0800

    created post board and get one board

--- app/routes.py
+++ app/routes.py
@@ -14,10 +14,36 @@ def read_boards():
   for board in boards:
     boards_response.append(
       {
-        "id": board.board_id,
+        # change 'id' to 'board_id'
+        "board_id": board.board_id,
         "title": board.title,
         "owner": board.owner
       }
     )
-    
   return jsonify(boards_response)
+
+
+#lets try to search by title rather than id
+@boards_bp.route('/<board_id>', methods=['GET'])
+def read_one_board(board_id):
+  board_id = int(board_id)
+  board = Board.query.get(board_id)
+
+  return{
+      "board_id": board.board_id,
+      "title": board.title,
+      "owner": board.owner
+  }
+
+
+@boards_bp.route('', methods=['POST'])
+def create_board():
+  request_body = request.get_json()
+  new_board = Board(
+    title=request_body["title"],
+    owner=request_body["owner"]
+  )
+  db.session.add(new_board)
+  db.session.commit()
+
+  return make_response(jsonify(f"Board {new_board.title} successfully created"), 201)

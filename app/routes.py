b85b4923fceab5a2ad17df2de8dd8ad2bd3ec7dd
Valentina Ruiz <v.zruiz06@gmail.com>
2023-01-03 07:35:03 -0800

    created read all boards endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,23 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
+
+boards_bp = Blueprint('boards_bp', __name__, url_prefix='/boards')
+
+@boards_bp.route('', methods=['GET'])
+def read_boards():
+  boards = Board.query.all()
+
+  boards_response = []
+  for board in boards:
+    boards_response.append(
+      {
+        "id": board.board_id,
+        "title": board.title,
+        "owner": board.owner
+      }
+    )
+    
+  return jsonify(boards_response)

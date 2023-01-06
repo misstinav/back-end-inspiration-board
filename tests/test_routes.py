7aaf61c5a0de80c455896225529d89faf5070fc5
lisafeng <lisafenglisa@yahoo.com>
2023-01-05 21:38:07 -0500

    created one_board in conftest and completed test_delete_board and test_delete_board_not_found unit tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,3 +9,23 @@ def test_get_empty_board_list(client):
   #assert
   assert response.status_code == 200
   assert response_body == []
+
+def test_delete_board(client, one_board):
+  #act
+  response = client.delete("/boards/1")
+  response_body = response.get_json()
+
+  #assert
+  assert response.status_code == 200
+  assert response_body == "Board 1 successfully deleted"
+  assert Board.query.get(1) == None
+
+def test_delete_board_not_found(client):
+  #act
+  response = client.delete("/boards/1")
+  response_body = response.get_json()
+
+  #assert 
+  assert response.status_code == 404
+  assert response_body == {"message": "Board 1 not found"}
+  assert Board.query.all() == []
